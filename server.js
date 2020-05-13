2fb3c0a0dbcb00871aa37f11d1f704092185917d
Le Phu Sang <lephusangus@gmail.com>
2020-05-13 15:05:55 +0700

    add middle ware

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require("express");
 const dotenv = require("dotenv");
-//ROUTER
+//Route files
 const routerBootcamps= require("./routes/bootcamps")
-
+//Load env Vars
 dotenv.config({ path: "./config/config.env" });
-
+//Middleware 
+const logger= require("./middlewares/logger")
 var app = express();
-app.use('/api/v1/', routerBootcamps)
+
+app.use(logger)
+//Mount route
+app.use('/api/v1/',routerBootcamps)
 
 const PORT = process.env.PORT || 5000;
 
